34848c22c5ed413376025f5b2d52d406593c235e
Aaftab Pinjari <pinjari.aaftab@gmail.com>
2021-05-17 11:36:54 +0530

    completed the addressForm part of the checkoutForm

--- src/components/checkoutForm/checkout/Checkout.jsx
+++ src/components/checkoutForm/checkout/Checkout.jsx
@@ -1,18 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import useStyles from './styles'
 import { CssBaseline, Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
 import AddressForm from '../AddressForm';
 import PaymentForm from '../PaymentForm';
+import { commerce } from '../../../lib/commerce'
 
 const steps = ['Shipping Address', 'Payment Details'];
 
-const Checkout = () => {
+const Checkout = ({ cart }) => {
     const [activeStep, setActiveStep] = useState(0)
+    const [checkoutToken, setCheckoutToken] = useState(null)
     const classes = useStyles();
 
 
+    /* we need to generate new token as soon as cart changes */
+    useEffect(() => {
+        const generateToken = async () => {
+            try {
+                const token = await commerce.checkout.generateToken(cart.id, { type: 'cart' });
+                console.log(token)
+                setCheckoutToken(token)
+            } catch (error) {
+
+            }
+        }
+        generateToken();
+    }, [cart])
+
+
     const Form = () => {
-        return activeStep === 0 ? <AddressForm /> : <PaymentForm />
+        return activeStep === 0 ? <AddressForm checkoutToken={checkoutToken} /> : <PaymentForm />
 
     }
     //dummy Confirmation
@@ -33,7 +50,8 @@ const Checkout = () => {
                         )
                         )}
                     </Stepper>
-                    {activeStep === steps.length ? <Confirmation /> : <Form />}
+                    {/* checkout && condition because addressform was rendering before we get the checkoutTokenId */}
+                    {activeStep === steps.length ? <Confirmation /> : checkoutToken && <Form />}
                 </Paper>
             </main>
         </>
